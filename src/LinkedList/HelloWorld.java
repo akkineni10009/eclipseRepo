87f213320979baa9b24d3fb6ebb0a57ba45744db
Sriram <Sriram>
2016-11-07 17:39:12 +0530

    adding comment

--- src/LinkedList/HelloWorld.java
+++ src/LinkedList/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld{
     
     public static Node start = null;
     
-
+//Comment
      public static void main(String []args){
         //System.out.println("Hello World");
         insertBeginning(1);
